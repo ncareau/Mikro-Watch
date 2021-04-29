f5bea15f99ce08fda6d14974f8ca0f9dcd0dfb1e
Lars Erik <lars.erik@dystland.no>
2021-04-29 13:57:27 +0200

    Do reverse lookup and add dnsname tag

--- src/Entity/IpAccount.php
+++ src/Entity/IpAccount.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 class IpAccount
 {
     public $ip;
+    public $dnsname = "";
     public $down_byte = 0;
     public $down_packet = 0;
     public $up_byte = 0;
@@ -37,6 +38,26 @@ class IpAccount
     {
         $this->down_packet = $this->down_packet + $packets;
         $this->down_byte = $this->down_byte + $bytes;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function set_dnsname(string $ip)
+    {
+        if (getenv('DNS_REVERSE_LOOKUP') == 'true') {
+            # Reverse lookup. If no name is returned from DNS, getHostByAddr returns the IP
+            $dnsname = getHostByAddr($ip);
+
+            # Trim domain names only, not IP addresses if DNS_REVERSE_LOOKUP_TRIM_SUFFIX is true
+            if (!preg_match("/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/", $dnsname)
+                && getenv('DNS_REVERSE_LOOKUP_TRIM_SUFFIX') == 'true'
+            ) {
+                $dnsname = preg_replace("/\..*$/", '', $dnsname);
+            }
+
+            $this->dnsname = $dnsname;
+        }
     }
 
 
